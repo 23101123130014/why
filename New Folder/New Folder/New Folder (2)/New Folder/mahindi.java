2b597f0e0265732618cbad32f0a510ac0ab7b4b3
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-02 02:30:03 +0300

    Update mahindi.java

--- New Folder/New Folder/New Folder (2)/New Folder/mahindi.java
+++ New Folder/New Folder/New Folder (2)/New Folder/mahindi.java
@@ -1,14 +1,21 @@
 //let we learn about public and private method
 public class mahindi
 {
-static huru()
+static void huru()
 {
-  System.out.println("it is free to use  this methd without creating an object of the class");
+  System.out.println("it is free to use  this method without creating an object of the class");
+}
+
+public void fungwa()
+{
+  System.out.println("it is only called by creating the object of the class");
 }
 
 public static void main(String [] args)
 {
+  mahindi ob = new mahindi();
   huru();
+  ob.fungwa();
 }
 }
   
