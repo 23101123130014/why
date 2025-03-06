c31866d136f2d22590346093c7b52c8851a142ba
23101123130014 <jacobmwansambe1@gmail.com>
2025-03-06 17:25:56 +0300

    Create untitled2.py

---
+++ New Folder/New Folder/untitled2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  5 17:12:54 2025
+
+@author: JACOB MWANSAMBE
+"""
+
+x = 1j;
+y = 23j;
+z = 34j;
+
+print(x, type(x));
+
+print(y, type(y));
+
+print(z, type(z));
